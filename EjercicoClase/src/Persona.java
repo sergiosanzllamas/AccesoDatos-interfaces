feebb2066cdf268510658004ab2f2e6f4062e958
sergiosanzllamas <sanzsergio4ab@gmail.com>
2018-10-17 10:33:42 +0200

    ab

---
+++ EjercicoClase/src/Persona.java
@@ -0,0 +1,42 @@
+
+public class Persona {
+	String nombre;
+	String apellido;
+	int edad;
+
+	public Persona() {
+		nombre = "Sergio";
+		apellido = "Sanz Llamas";
+		edad = 19;
+	}
+
+	public Persona(String nombre, String apaellido, int edad) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+
+	public String escribr() {
+		return nombre;
+
+	}
+
+	public void datos() {
+		System.out.println("Nombre:" + nombre + "\n" + "Apellidos" + apellido + "\n" + "Edad:" + edad);
+	}
+
+	public boolean mayor() {
+		if (edad > 17) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public  int[] lon(String nobre, String apellido) {
+		int tam;
+		for()
+		return null;
+		
+	}
+
+}
